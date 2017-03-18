8f08f00b256703af6315167c8f7f7ded27b8e539
Raul Jordan <raulitojordan@gmail.com>
2017-03-18 16:30:57 -0400

    fix

--- src/home/components/Home.js
+++ src/home/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Page from '../../shared/components/Page';
 import Hero from '../../shared/components/Hero';
 import Header from '../../shared/components/Header';
 import Button from '../../shared/components/Button';
+
 function Home() {
   return (
-    <Page>
+    <div>
       <div className="intro">
         <Header><span>Retro</span> is a nostalgia trip through time, right in the feels</Header>
         <p>Explore all sorts of retro music, videos, gifs, and all sorts of other radical things we know you miss in 2017.</p>
@@ -14,7 +14,7 @@ function Home() {
         Ride the wave with us and explore Vaporwave music from around the world
       </Hero>
       <Button>Check It Out</Button>
-    </Page>
+    </div>
   );
 }
 
--- src/shared/components/Page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Page = styled.div`
-  margin: 0 auto;
-  max-width: 1000px;
-`;
-
-export default Page;
