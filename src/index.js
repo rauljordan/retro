b6b4e4bb14bd345591ac81e29c477f7fd07f9365
Raul Jordan <raulitojordan@gmail.com>
2017-03-18 16:37:26 -0400

    browser

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
 ReactDOM.render((
-  <Router key={Math.random()} onUpdate={() => window.scrollTo(0, 0)}>
+  <Router history={browserHistory} key={Math.random()} onUpdate={() => window.scrollTo(0, 0)}>
     {routes}
   </Router>
 ), document.getElementById('app'));
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV == 'development' && module.hot) {
   module.hot.accept('./routes', () => {
     const newRoutes = require('./routes').default;
     ReactDOM.render((
-      <Router key={Math.random()} onUpdate={() => window.scrollTo(0, 0)}>
+      <Router history={browserHistory} key={Math.random()} onUpdate={() => window.scrollTo(0, 0)}>
         {newRoutes}
       </Router>
     ), document.getElementById('app'));
